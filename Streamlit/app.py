20930dd661141a8eb51565255e0b5845bb0b1d84
IAnthonyRenard <contact.anthony.renard@gmail.com>
2023-09-07 09:34:54 +0200

    Mise à jour de app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -39,8 +39,8 @@ def main():    #Fonction principale qui contient l'application
         #folder_path2 = "fire_prediction_images_no_yes2"
         
         #Lien pour Streamlit
-        folder_path1 = "input/fire_prediction_images_no_yes"
-        folder_path2 = "Streamlit/fire_prediction_images_no_yes"
+        folder_path1 = "/input/fire_prediction_images_no_yes"
+        folder_path2 = "/Streamlit/fire_prediction_images_no_yes"
         
         
         no_images = os.listdir(folder_path2 + '/no/')
